92b06aabdde7289ebb7656c923279ef5d7f55342
Oscar Tinajero <tinajero117.oscar@gmail.com>
2022-11-08 09:10:27 -0600

    Finish v1

--- lib/app/modules/detail/page.dart
+++ lib/app/modules/detail/page.dart
@@ -17,138 +17,141 @@ class DetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final task = homeController.task.value!;
     final color = HexColor.fromHex(task.color);
-    return Scaffold(
-      body: Form(
-        key: homeController.formKey,
-        child: ListView(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(3.0.wp),
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Get.back();
-                      homeController.updateTodos();
-                      homeController.changeTask(null);
-                      homeController.editController.clear();
-                    },
-                    icon: const Icon(Icons.arrow_back),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0.wp),
-              child: Row(
-                children: [
-                  Icon(
-                    IconData(
-                      task.icon,
-                      fontFamily: 'MaterialIcons',
-                    ),
-                    color: color,
-                  ),
-                  SizedBox(width: 3.0.wp),
-                  Text(
-                    task.title,
-                    style: TextStyle(
-                      fontSize: 12.0.sp,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Obx(
-              () {
-                final totalTodos = homeController.doingTodos.length +
-                    homeController.doneTodos.length;
-                return Padding(
-                  padding: EdgeInsets.only(
-                    left: 16.0.wp,
-                    top: 3.0.wp,
-                    right: 16.0.wp,
-                  ),
-                  child: Row(
-                    children: [
-                      Text(
-                        '$totalTodos Tasks',
-                        style: TextStyle(
-                          fontSize: 12.0.sp,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      SizedBox(width: 3.0.wp),
-                      Expanded(
-                        child: StepProgressIndicator(
-                          totalSteps: totalTodos == 0 ? 1 : totalTodos,
-                          currentStep: homeController.doneTodos.length,
-                          size: 5,
-                          padding: 0,
-                          selectedGradientColor: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [color.withOpacity(0.5), color],
-                          ),
-                          unselectedGradientColor: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                              Colors.grey.shade300,
-                              Colors.grey.shade300
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: 2.0.wp,
-                horizontal: 5.0.wp,
-              ),
-              child: TextFormField(
-                controller: homeController.editController,
-                autofocus: true,
-                decoration: InputDecoration(
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey.shade400),
-                  ),
-                  prefixIcon: Icon(
-                    Icons.check_box_outline_blank,
-                    color: Colors.grey.shade400,
-                  ),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      if (homeController.formKey.currentState!.validate()) {
-                        final success = homeController
-                            .addTodo(homeController.editController.text);
-                        if (success) {
-                          EasyLoading.showSuccess('Todo item add success');
-                        } else {
-                          EasyLoading.showError('Todo item alrady exist');
-                        }
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: Form(
+          key: homeController.formKey,
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(3.0.wp),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Get.back();
+                        homeController.updateTodos();
+                        homeController.changeTask(null);
                         homeController.editController.clear();
-                      }
-                    },
-                    icon: const Icon(Icons.done),
-                  ),
+                      },
+                      icon: const Icon(Icons.arrow_back),
+                    ),
+                  ],
                 ),
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Please enter your todo item';
-                  }
-                  return null;
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0.wp),
+                child: Row(
+                  children: [
+                    Icon(
+                      IconData(
+                        task.icon,
+                        fontFamily: 'MaterialIcons',
+                      ),
+                      color: color,
+                    ),
+                    SizedBox(width: 3.0.wp),
+                    Text(
+                      task.title,
+                      style: TextStyle(
+                        fontSize: 12.0.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Obx(
+                () {
+                  final totalTodos = homeController.doingTodos.length +
+                      homeController.doneTodos.length;
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: 16.0.wp,
+                      top: 3.0.wp,
+                      right: 16.0.wp,
+                    ),
+                    child: Row(
+                      children: [
+                        Text(
+                          '$totalTodos Tasks',
+                          style: TextStyle(
+                            fontSize: 12.0.sp,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(width: 3.0.wp),
+                        Expanded(
+                          child: StepProgressIndicator(
+                            totalSteps: totalTodos == 0 ? 1 : totalTodos,
+                            currentStep: homeController.doneTodos.length,
+                            size: 5,
+                            padding: 0,
+                            selectedGradientColor: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [color.withOpacity(0.5), color],
+                            ),
+                            unselectedGradientColor: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Colors.grey.shade300,
+                                Colors.grey.shade300
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
                 },
               ),
-            ),
-            DoingList(),
-            DoneList(),
-          ],
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 2.0.wp,
+                  horizontal: 5.0.wp,
+                ),
+                child: TextFormField(
+                  controller: homeController.editController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.check_box_outline_blank,
+                      color: Colors.grey.shade400,
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        if (homeController.formKey.currentState!.validate()) {
+                          final success = homeController
+                              .addTodo(homeController.editController.text);
+                          if (success) {
+                            EasyLoading.showSuccess('Todo item add success');
+                          } else {
+                            EasyLoading.showError('Todo item alrady exist');
+                          }
+                          homeController.editController.clear();
+                        }
+                      },
+                      icon: const Icon(Icons.done),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter your todo item';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              DoingList(),
+              DoneList(),
+            ],
+          ),
         ),
       ),
     );
--- lib/app/modules/report/page.dart
+++ lib/app/modules/report/page.dart
@@ -1,14 +1,170 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
+
+import '../../core/values/colors.dart';
+import '/app/core/utils/extensions.dart';
+import '../home/controller.dart';
 
 class ReportPage extends StatelessWidget {
-  const ReportPage({Key? key}) : super(key: key);
+  final homeController = Get.find<HomeController>();
+  ReportPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Report page'),
+    return Scaffold(
+      body: SafeArea(
+        child: Obx(
+          () {
+            final createdTasks = homeController.getTotalTask();
+            final completedTasks = homeController.getTotalDoneTask();
+            final liveTasks = createdTasks - completedTasks;
+            final percent =
+                (completedTasks / createdTasks * 100).toStringAsFixed(0);
+            return ListView(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(4.0.wp),
+                  child: Text(
+                    'My Report',
+                    style: TextStyle(
+                      fontSize: 24.0.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 4.0.wp),
+                  child: Text(
+                    DateFormat.yMMMMd().format(DateTime.now()),
+                    style: TextStyle(
+                      fontSize: 14.0.sp,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 3.0.wp,
+                    horizontal: 4.0.wp,
+                  ),
+                  child: const Divider(thickness: 2),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 3.0.wp,
+                    horizontal: 5.0.wp,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildStatus(
+                        color: Colors.green,
+                        number: liveTasks,
+                        title: 'Live Tasks',
+                      ),
+                      _buildStatus(
+                        color: Colors.orange,
+                        number: completedTasks,
+                        title: 'Completed Tasks',
+                      ),
+                      _buildStatus(
+                        color: Colors.blue,
+                        number: createdTasks,
+                        title: 'Created Tasks',
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 8.0.wp),
+                UnconstrainedBox(
+                  child: SizedBox(
+                    width: 70.0.wp,
+                    height: 70.0.wp,
+                    child: CircularStepProgressIndicator(
+                      totalSteps: createdTasks == 0 ? 1 : createdTasks,
+                      currentStep: completedTasks,
+                      stepSize: 20,
+                      selectedColor: green,
+                      unselectedColor: Colors.grey.shade200,
+                      padding: 0,
+                      width: 150,
+                      height: 150,
+                      selectedStepSize: 22,
+                      roundedCap: (p0, p1) => true,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '${createdTasks == 0 ? 0 : percent} %',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0.sp,
+                            ),
+                          ),
+                          SizedBox(height: 1.0.wp),
+                          Text(
+                            'Efficiency',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12.0.sp,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
+
+  Row _buildStatus({
+    required Color color,
+    required int number,
+    required String title,
+  }) =>
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 3.0.wp,
+            width: 3.0.wp,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                width: 0.5.wp,
+                color: color,
+              ),
+            ),
+          ),
+          SizedBox(width: 3.0.wp),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '$number',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0.sp,
+                ),
+              ),
+              SizedBox(height: 2.0.wp),
+              Text(
+                title,
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 12.0.sp,
+                ),
+              ),
+            ],
+          )
+        ],
+      );
 }
